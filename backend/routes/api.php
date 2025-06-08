8cf4e9ffa0bf21965e69e1a8485c2013c4d1c640
ebacuadostefan <ebacuadomentino@gmail.com>
2025-06-08 15:13:37 +0800

    categories

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +13,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
-
+Route::apiResource('categories', CategoryController::class);
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\CashierController;
+// use App\Http\Controllers\CashierController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('admin/dashboard', [HomeController::class, 'index'])->
+route::get('admin/dashboard', [AdminController::class, 'index'])->
     middleware(['auth','admin']);
-route::get('cashier/dashboard', [CashierController::class, 'index']);
+// route::get('cashier/dashboard', [CashierController::class, 'index']);
+
+Route::resource('categories', CategoryController::class);
+
